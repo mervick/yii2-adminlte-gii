2314fbfb69af104cf0a6454ce32f7ba734755967
Andrey Izman <izman.dev@gmail.com>
2015-06-29 18:40:21 +0300

    Modify the fields positions

--- generators/model/form.php
+++ generators/model/form.php
@@ -4,17 +4,17 @@
 /* @var $generator mervick\adminlte\gii\generators\model\Generator */
 
 echo $form->field($generator, 'tableName');
+echo $form->field($generator, 'useTablePrefix')->checkbox();
 echo $form->field($generator, 'modelClass');
 echo $form->field($generator, 'ns');
 echo $form->field($generator, 'baseClass');
+echo $form->field($generator, 'generateQuery')->checkbox();
+echo $form->field($generator, 'queryClass');
+echo $form->field($generator, 'queryNs');
+echo $form->field($generator, 'queryBaseClass');
 echo $form->field($generator, 'db');
-echo $form->field($generator, 'useTablePrefix')->checkbox();
 echo $form->field($generator, 'generateRelations')->checkbox();
 echo $form->field($generator, 'generateLabelsFromComments')->checkbox();
-echo $form->field($generator, 'generateQuery')->checkbox();
-echo $form->field($generator, 'queryNs');
-echo $form->field($generator, 'queryClass');
-echo $form->field($generator, 'queryBaseClass');
 echo $form->field($generator, 'enableI18N')->checkbox();
 echo $form->field($generator, 'messageCategory');
 echo $form->field($generator, 'addingI18NStrings')->checkbox();
