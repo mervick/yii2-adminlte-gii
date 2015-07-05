9a9649dea4280640fa8d5ec0a39af495ca04af0a
Andrey Izman <izman.dev@gmail.com>
2015-07-05 13:35:47 +0300

    Fix namespace

--- generators/crud/default/views/_form.php
+++ generators/crud/default/views/_form.php
@@ -4,7 +4,7 @@ use yii\helpers\Inflector;
 use yii\helpers\StringHelper;
 
 /* @var $this yii\web\View */
-/* @var $generator backend\gii\generators\crud\Generator */
+/* @var $generator mervick\adminlte\gii\generators\crud\Generator */
 
 /* @var $model \yii\db\ActiveRecord */
 $model = new $generator->modelClass();
